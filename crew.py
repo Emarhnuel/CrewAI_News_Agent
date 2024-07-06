9cd92123c323627078a848ff713a4085d28e1f7a
Emmanuel Ezeokeke <147157655+Emarhnuel@users.noreply.github.com>
2024-07-06 15:38:46 +0100

    Update crew.py

--- crew.py
+++ crew.py
@@ -1,16 +1,23 @@
 from crewai import Crew,Process
 from tasks import research_task,write_task
 from agents import news_researcher,news_writer
+import agentops
+
+agentops.init(tags=["crewai-agent"])
+
 
 ## Forming the tech focused crew with some enhanced configuration
 crew=Crew(
     agents=[news_researcher,news_writer],
     tasks=[research_task,write_task],
     process=Process.sequential,
+    verbose=2,
 
 )
 
 ## starting the task execution process wiht enhanced feedback
 
-result=crew.kickoff(inputs={'topic':'AI in healthcare'})
+result=crew.kickoff(inputs={'topic':'AI in Fintech'})
 print(result)
+
+agentops.end_session("Success")
